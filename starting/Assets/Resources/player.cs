b1512b074b12a31a6958e2988e40ea7ff6a0ec21
Juliana Potengy <jupotengy@gmail.com>
2016-06-14 10:36:38 -0300

    b

---
+++ starting/Assets/Resources/player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	Camera cam;
+	Transform my;
+	Rigidbody2D body;
+
+	private float Speed = 0.1f;
+	
+	void Awake ()
+	{
+		cam = Camera.main;
+		my = GetComponent <Transform> ();
+		body = GetComponent <Rigidbody2D> ();
+	}
+	
+	
+	void Update ()
+	{
+		if(Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(Vector3.up * Speed);
+		}
+		if(Input.GetKey(KeyCode.S))
+		{
+			transform.Translate(Vector3.down * Speed);
+		}
+
+		// Distance from camera to object.  We need this to get the proper calculation.
+		float camDis = cam.transform.position.x - my.position.x;
+		
+		// Get the mouse position in world space. Using camDis for the Z axis.
+		Vector3 mouse = cam.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, camDis));
+		
+		float AngleRad = Mathf.Atan2 (-mouse.x - -my.position.x, mouse.y - my.position.y);
+		float angle = (180 / Mathf.PI) * AngleRad;
+		
+		body.rotation = angle;
+	}
+}
